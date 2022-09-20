702617876dc9a4263262883bb5b3bfbde0d1d350
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2022-09-20 20:58:48 +0430

    i add the script for making carousel ready for my project

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
 // i dont want to move from the first slide to the last slide
 var CarouselControlPrev = document.getElementById("previous__button");
-var QuestionsContainerChildren = document.getElementById("#questions__container").children;
-console.log(QuestionsContainerChildren.item[2])
+var QuestionsContainerChildren = document.getElementById("questions__container").children;
+console.log(QuestionsContainerChildren.length)
 var CarouselControlNext = document.getElementById("next__button");
 
-// CarouselControlPrev.addEventListener("click" , isTheFirstSlide);
-// CarouselControlNext.addEventListener("click" , isTheLastSlide);
+CarouselControlPrev.addEventListener("click" , isTheFirstSlide);
+CarouselControlNext.addEventListener("click" , isTheLastSlide);
 
-// function isTheFirstSlide(){
-//     var isTheFirstSlide = CheckThatIsFirst();
-//     if(isTheFirstSlide == false) return;
-//     CarouselControlPrev.disabled = true;
-// }
-// function isTheLastSlide(){
-//     var isTheLastSlide = CheckThatIsLast();
-//     CarouselControlPrev.disabled = false;
-//     if(isTheLastSlide == false) return;
-//     CarouselControlNext.disabled = true;
-// }
+function isTheFirstSlide(){
+    var isTheFirstSlide = CheckThatIsFirst();
+    CarouselControlNext.disabled = false;
+    if(isTheFirstSlide == false) return;
+    CarouselControlPrev.disabled = true;
+}
+function isTheLastSlide(){
+    var isTheLastSlide = CheckThatIsLast();
+    CarouselControlPrev.disabled = false;
+    if(isTheLastSlide == false) return;
+    CarouselControlNext.disabled = true;
+}
 
-// function CheckThatIsFirst(){
-//     var secondChild = QuestionsContainerChildren.item(1);
-//     console.log(secondChild)
-//     if(secondChild.classList.contains("active")){
-//         return true;
-//     }else{
-//         return false
-//     }
-// }
+function CheckThatIsFirst(){
+    var secondChild = QuestionsContainerChildren.item(1);
+    if(secondChild.classList.contains("active")){
+        return true;
+    }else{
+        return false
+    }
+}
 function CheckThatIsLast(){
-    // var LastChild = QuestionsContainer.lastElementChild;
-    // console.log(LastChild)
-    // if(LastChild.classList.contains("active")){
-    //     return true;
-    // }else{
-    //     return false
-    // }
-    return false
+    var OneLeftLastChild = QuestionsContainerChildren.item(QuestionsContainerChildren.length - 2);
+    if(OneLeftLastChild.classList.contains("active")){
+        return true;
+    }else{
+        return false
+    }
 }
